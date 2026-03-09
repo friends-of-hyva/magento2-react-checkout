b7f23fa5c60c234a7549a47840832b6db0fc34c3
Rajeev K Tomy <rajeevphpdeveloper@gmail.com>
2026-03-09 18:26:12 +0100

    Update react depedencies and update tailwind css to 4x

--- src/reactapp/vite.config.js
+++ src/reactapp/vite.config.js
@@ -1,13 +1,15 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // includes app env settings into process.env;
-  // Only settings starts with `HYVA_` works
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), ['HYVA_']) };
+export default defineConfig(async ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'HYVA_')
+
+  const plugins = [react()]
+
+  if (mode === 'production') {
+    const { visualizer } = await import('rollup-plugin-visualizer')
+    plugins.push(visualizer())
+  }
 
   const newConfig = {
     build: {
@@ -17,19 +19,19 @@ export default defineConfig(({ mode }) => {
       emptyOutDir: false,
     },
     envPrefix: 'HYVA_',
-    plugins: [react()],
+    plugins,
     server: {
       port: 3000,
       proxy: {
         '/backend': {
-          target: process.env.HYVA_BASE_URL || 'https://demo.hyva.io',
+          target: env.HYVA_BASE_URL || 'https://demo.hyva.io',
           changeOrigin: true,
           secure: false,
           rewrite: (path) => path.replace(/^\/backend/, ''),
         },
       },
     },
-  };
+  }
 
   if (mode === 'production') {
     return {
@@ -41,35 +43,23 @@ export default defineConfig(({ mode }) => {
             entryFileNames: 'js/react-checkout.js',
             chunkFileNames: 'js/[name].js',
             assetFileNames: (assetInfo) => {
-              if (assetInfo.name === 'style.css') {
-                return 'css/styles.css';
+              if (assetInfo.name?.endsWith('.css')) {
+                return 'css/styles.css'
               }
-              return assetInfo.name;
+              return assetInfo.name || '[name][extname]'
             },
           },
         },
       },
-      // Visualizing the build. You will see stats.html file at the root and there by analyze the build
-      plugins: [...newConfig.plugins, visualizer()],
       resolve: {
         alias: {
-          /**
-           * There are some core libraries such as formik, yup etc. uses lodash
-           * library and hence using lodash-es gives best bundle result. Not a good
-           * idea to rely on 'lodash' directly as it will include both lodash and lodash-es
-           * in the build. So we uses lodash-es and by aliasing lodash => lodash-es, we will
-           * get the most optimized build.
-           */
           lodash: 'lodash-es',
-          /**
-           * Use preact instead of react to improve the build size.
-           */
           react: 'preact/compat',
           'react-dom': 'preact/compat',
         },
       },
-    };
+    }
   }
 
-  return newConfig;
-});
+  return newConfig
+})
